f3f67607ec90e7388ffa13cd7d59b5711f63e593
stetsiv <ystetsiv@xlntech.co.uk>
2018-09-15 12:10:43 +0300

    [Refactor] WebAssertion class

--- src/main/java/core/modules/web/models/WebAssertion.java
+++ src/main/java/core/modules/web/models/WebAssertion.java
@@ -3,7 +3,6 @@ package core.modules.web.models;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,7 +10,7 @@ import java.util.List;
 public class WebAssertion {
 
     private WebElement elementForAssert;
-    private List <WebElement> elementListForAssert;
+    private List<WebElement> elementListForAssert;
 
     public WebAssertion(WebElement elementForAssert) {
         this.elementForAssert = elementForAssert;
@@ -21,18 +20,22 @@ public class WebAssertion {
         this.elementListForAssert = elementListForAssert;
     }
 
-    public void shouldBeEnabled(boolean condition) {
-        Assert.assertTrue(elementForAssert.isEnabled(), "Element is not enabled");
+    public void shouldBeEnabled(boolean condition) throws Exception {
+        throw new Exception("Method isn't support");
     }
 
     public void shouldBeDisplayed(boolean condition) {
-        Assert.assertTrue(elementForAssert.isDisplayed(), "Element is not displayed");
+        if (condition) {
+            Assert.assertTrue(elementForAssert.isDisplayed(), "Element " + elementForAssert.toString() + " is not displayed");
+        } else {
+            Assert.assertFalse(elementForAssert.isDisplayed(), "Element " + elementForAssert.toString() + " is displayed");
+        }
     }
 
     public void elementListShouldBeDisplayed(boolean condition) {
         SoftAssert softAssert = new SoftAssert();
         for (WebElement element : elementListForAssert) {
-            if(condition){
+            if (condition) {
                 softAssert.assertTrue(element.isDisplayed(), "Element " + element.toString() + " is not displayed");
             } else {
                 softAssert.assertFalse(element.isDisplayed(), "Element " + element.toString() + " is displayed");
@@ -41,66 +44,73 @@ public class WebAssertion {
         softAssert.assertAll();
     }
 
-     public void shouldBeImage(boolean condition) throws Exception{
+    public void shouldBeImage(boolean condition) throws Exception {
         throw new Exception("Method isn't support");
     }
 
     public void shouldBeSelected(boolean condition) {
-        if(condition){
-            Assert.assertTrue(elementForAssert.isSelected(), "Element is not selected");
+        if (condition) {
+            Assert.assertTrue(elementForAssert.isSelected(), "Element " + elementForAssert.toString() + " is not selected");
         } else {
-            Assert.assertFalse(elementForAssert.isSelected(), "Element is selected");
+            Assert.assertFalse(elementForAssert.isSelected(), "Element " + elementForAssert.toString() + " is selected");
         }
     }
 
-    public void labelListShouldContainText (String ... expectedText) {
+    public void elementListShouldContainText(String... expectedText) {
         checkTextListForEqual(expectedText);
     }
 
-    public void fieldShouldContainText (String attributeName, String ... expectedText) {
-        checkAttributeValueTextForEqual (attributeName, expectedText);
+    public void fieldShouldContainText (String attributeName, Object value) {
+        checkAttributeValueTextForEqual (attributeName, value);
     }
 
-    public void attributeValueListShouldContainText (String attributeName, String ... expectedText) {
-        checkAttributeValueTextForEqual (attributeName, expectedText);
-    }
+    private void checkTextListForEqual (Object value) {
+        if (value instanceof String) {
+            String actualValue = elementForAssert.getText().toLowerCase().trim();
+            String expectedValue = ((String) value).toLowerCase().trim();
+            Assert.assertEquals(actualValue, expectedValue, "Text " + actualValue + " is not present");
+        } else if (value instanceof String[]) {
+            ArrayList<String> actualValueList = new ArrayList<>();
+            ArrayList<String> expectedValues = convertToLowerCase(new ArrayList<>(Arrays.asList((String[]) value)));
 
-    private void checkTextListForEqual (String ... expectedText) {
-        ArrayList<String> actualValues = new ArrayList<>();
-        ArrayList<String> expectedValues = convertToLowerCase(new ArrayList<>(Arrays.asList(expectedText)));
+            for (WebElement element : elementListForAssert) {
+                String label = element.getText().toLowerCase();
+                actualValueList.add(label.toLowerCase());
+            }
 
-        for (WebElement elemnent : elementListForAssert) {
-            String label = elemnent.getText().toLowerCase();
-            actualValues.add(label.toLowerCase());
+            List<String> sourceList = new ArrayList<String>(expectedValues);
+            List<String> destinationList = new ArrayList<String>(actualValueList);
+            sourceList.removeAll(actualValueList);
+            destinationList.removeAll(expectedValues);
+
+            System.out.println(actualValueList.toString());
+            System.out.println(expectedValues.toString());
+
+            Assert.assertEquals(actualValueList.toString(), expectedValues.toString(), "Text" + sourceList.toString() + " are not present");
         }
-
-        List<String> sourceList = new ArrayList<String>(expectedValues);
-        List<String> destinationList = new ArrayList<String>(actualValues);
-
-        sourceList.removeAll(actualValues);
-        destinationList.removeAll(expectedValues);
-
-        Assert.assertEquals(actualValues.toString(), expectedValues.toString(), "Labels " + sourceList.toString() + " are not present");
     }
 
-    private void checkAttributeValueTextForEqual (String attributeName, String ... expectedText) {
-        ArrayList<String> actualLabels = new ArrayList<>();
-        ArrayList<String> expectedValues = convertToLowerCase(new ArrayList<>(Arrays.asList(expectedText)));
+    private void checkAttributeValueTextForEqual (String attributeName, Object value) {
+        if (value instanceof String){
+            String actualValue = elementForAssert.getAttribute(attributeName).toLowerCase().trim();
+            String expectedValue = ((String) value).toLowerCase();
+            Assert.assertEquals(actualValue, expectedValue, "Text " + actualValue + " is not present");
+        } else if (value instanceof String []){
+            ArrayList<String> actualValueList = new ArrayList<>();
+            ArrayList<String> expectedValues = convertToLowerCase(new ArrayList<>(Arrays.asList((String [])value)));
 
-        for (WebElement elemnent : elementListForAssert) {
-            String label = elemnent.getAttribute(attributeName).toLowerCase();
-            actualLabels.add(label.toLowerCase());
+            for (WebElement elemnent : elementListForAssert) {
+                String label = elemnent.getAttribute(attributeName).toLowerCase();
+                actualValueList.add(label.toLowerCase());
+            }
+
+            List<String> sourceList = new ArrayList<String>(expectedValues);
+            List<String> destinationList = new ArrayList<String>(actualValueList);
+            sourceList.removeAll(actualValueList);
+            destinationList.removeAll(expectedValues);
+            Assert.assertEquals(actualValueList.toString(), expectedValues.toString(), "Text" + sourceList.toString() + " are not present");
         }
-
-        List<String> sourceList = new ArrayList<String>(expectedValues);
-        List<String> destinationList = new ArrayList<String>(actualLabels);
-
-        sourceList.removeAll(actualLabels);
-        destinationList.removeAll(expectedValues);
-
-        Assert.assertEquals(actualLabels.toString(), expectedValues.toString(), "Labels " + sourceList.toString() + " are not present");
     }
-
 
     private ArrayList<String> convertToLowerCase (ArrayList<String> list) {
         ArrayList<String> lowerCaseList  = new ArrayList<>();
@@ -109,5 +119,4 @@ public class WebAssertion {
         }
         return lowerCaseList;
     }
-
 }
